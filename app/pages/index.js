588057c9d3c0f218d53e997a20806bb4de0038fc
Wilko Nienhaus <wilko.nienhaus@vaimo.com>
2017-12-03 08:27:48 +0100

    small fixes. also remove dependency on Mongo, which we dont actually use

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,5 @@
 export { default as App } from 'pages/App';
-export { default as Search } from 'pages/search';
+export { default as Search } from 'pages/Search';
 export { default as Dashboard } from 'pages/Dashboard';
 export { default as About } from 'pages/About';
 export { default as Carrier } from 'pages/Carrier';
